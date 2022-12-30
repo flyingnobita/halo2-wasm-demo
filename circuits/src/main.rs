4018a0666db89116ffaa4c966b3965ab4a7dc96c
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2022-12-30 15:10:33 +0800

    Move halo2 circuits to "circuits" folder

--- circuits/src/main.rs
+++ circuits/src/main.rs
@@ -5,6 +5,12 @@ use halo2_proofs::{circuit::Value, dev::MockProver, pasta::Fp};
 use halo2_playground_circuits::circuit_1;
 use halo2_playground_circuits::circuit_2;
 
+#[cfg(target_family = "wasm")]
+pub use wasm_bindgen_rayon::init_thread_pool;
+
+#[cfg(target_family = "wasm")]
+extern crate console_error_panic_hook;
+
 fn run_circuit_1(input_num: u64) {
     let k = 3;
 
@@ -17,7 +23,7 @@ fn run_circuit_1(input_num: u64) {
 
     let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
     prover.assert_satisfied();
-    println!("Circuit 1 Proven Output: {:?}", public_inputs);
+    println!("Circuit 1 Verified Output: {:?}", public_inputs);
 }
 
 fn run_circuit_2(input_num: u64) {
@@ -32,10 +38,20 @@ fn run_circuit_2(input_num: u64) {
 
     let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
     prover.assert_satisfied();
-    println!("Circuit 2 Proven Output: {:?}", public_inputs);
+    println!("Circuit 2 Verified Output: {:?}", public_inputs);
 }
 
 fn main() {
+    // #[wasm_bindgen]
+    // pub async fn prove_play(final_word: String, words_js: JsValue, params_ser: JsValue) -> JsValue {
+    //     // Steps:
+    //     // - Deserialise function parameters
+    //     // - Generate the instance and advice columns using the words
+    //     // - Instantiate the circuit and generate the witness
+    //     // - Generate the proving key from the params
+    //     // - Create a proof
+    // }
+
     let input = get_user_input();
 
     run_circuit_1(input);
